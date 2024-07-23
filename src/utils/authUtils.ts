6dcb17bafcc9d803e9c73a260bee4a2c26bb4733
Shreyash Kumar <shreyashgaur1@gmail.com>
2024-07-23 17:27:58 +0530

    Changes

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -2,14 +2,14 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import express, { NextFunction, Response } from "express"
 import { UserRequest } from "./interface_Types"
 export default class Auth {
-    constructor() {}
+    constructor() { }
     async generateToken(id: string): Promise<string> {
         return jwt.sign({ id }, process.env.JWT_SECRET!, {
-            expiresIn: parseInt(process.env.JWT_EXPIRES_IN || '1') * 24 *60 *60 *1000,
+            expiresIn: parseInt(process.env.JWT_EXPIRES_IN || '1') * 24 * 60 * 60 * 1000,
         });
     }
 
-    async headerAuthToken(res: express.Response, user: string,): Promise<{ success: boolean, token:string }> {
+    async headerAuthToken(res: express.Response, user: string,): Promise<{ success: boolean, token: string }> {
         // Token Generation
         const token = await this.generateToken(user);
         const jwtCookieExpiresIn = process.env.JWT_COOKIE_EXPIRES_IN;
@@ -18,7 +18,7 @@ export default class Auth {
         }
 
         // Cookie validation days setup
-        const options:any = {
+        const options: any = {
             expires: new Date(
                 Date.now() + parseInt(jwtCookieExpiresIn) * 24 * 60 * 1000
             ),
@@ -33,34 +33,39 @@ export default class Auth {
         return { success: true, token };
     }
 
-    async tokenVerificationForGoogle(token:string): Promise<{success: boolean, id: string|undefined}> {
+    async tokenVerificationForGoogle(token: string): Promise<{ success: boolean, id: string | undefined }> {
 
-        // Returning if no token
-        if (!token) {
-            return {
-                success: false,
-                id: undefined
+        try {
+            // Returning if no token
+            if (!token) {
+                return {
+                    success: false,
+                    id: undefined
+                }
+            }
+
+            // Decoding user using token
+            type DecodedJwt = {
+                exp: number; // Assuming expiration is stored in 'exp' claim
+                id: string;
+                iat: number
+            };
+            const decoded = await jwt.verify(token, process.env.JWT_SECRET!) as DecodedJwt;
+
+            if (typeof decoded === 'string') {
+                return {
+                    success: false,
+                    id: undefined
+                }
+            } else {
+                return {
+                    success: true,
+                    id: decoded.id
+                }
             }
         }
-
-        // Decoding user using token
-        type DecodedJwt = {
-            exp: number; // Assuming expiration is stored in 'exp' claim
-            id: string;
-            iat: number
-        };
-        const decoded = await jwt.verify(token, process.env.JWT_SECRET!) as DecodedJwt;
-
-        if (typeof decoded === 'string') {
-            return {
-                success: false,
-                id: undefined
-            }
-        } else {
-            return {
-                success: true,
-                id: decoded.id
-            }
+        catch (err) {
+            return { success: false, id: 'Not valid credentials' }
         }
     }
 }
