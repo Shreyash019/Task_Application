6e13ea04bd028aaae57906f158c0267795271d99
Shreyash Kumar <shreyashgaur1@gmail.com>
2024-07-23 23:30:44 +0530

    Server Changes

--- Server/dist/utils/RequestMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const attachUserToRequest = (req, res, next) => {
-    req.user = req.user;
-    next();
-};
-exports.default = attachUserToRequest;
