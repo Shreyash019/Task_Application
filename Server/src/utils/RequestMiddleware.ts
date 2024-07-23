61ec2e5f3c8e2770956a8418e74ef625607b79b4
Shreyash Kumar <shreyashgaur1@gmail.com>
2024-07-23 23:25:34 +0530

    Changes

--- Server/src/utils/RequestMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { UserRequest } from "./interface_Types";
-
-const attachUserToRequest = (req: UserRequest, res: Response, next: NextFunction) => {
-    (req as Request).user = req.user;
-    next();
-};
-
-export default attachUserToRequest;
